cd1dcd9af20a39c1bb360e767131991c29110f2d
Souad <souad.kassimi.2022@.gmail.com>
2024-03-26 01:42:20 +0000

    middleware

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,13 @@ Route::resource('patient',patientController::class);
 Route::resource('rendezvous',rendezvousController::class);
 
 //routes de admin
-Route::resource('admin',adminController::class);
+Route::resource('admin',adminController::class)->middleware('auth');
 //routes de login
 Route::post('/login/singin',[login::class,'login'])->name('login.signin');
 Route::get('/login',[login::class,'index'])->name('login.index');
 Route::get('/logout',[login::class,'logout'])->name('login.logout');
 //routes de dates
 Route::resource('dateNonDispo',dates_non_dispoController::class);
+
+
 
